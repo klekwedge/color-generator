797ac9bcc23466732581871bf740cd4767cc792b
Andrey <antonov.andrey.10101@gmail.com>
2022-11-22 12:11:51 +0300

    feat: add the ability to copy color

--- src/components/Color/Color.tsx
+++ src/components/Color/Color.tsx
@@ -1,25 +1,57 @@
-import { Flex } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Flex, Heading } from "@chakra-ui/react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 
 interface ColorProps {
   hex: string;
 }
 
 function Color({ hex }: ColorProps) {
+  const [copyInfo, setCopyInfo] = useState(false);
+
+  const copyToBuffer = (e: BaseSyntheticEvent) => {
+    navigator.clipboard.writeText(e.target.innerText).catch((err) => {
+      console.error("Error in copying text: ", err);
+    });
+
+    setCopyInfo(true);
+  };
+
+  useEffect(() => {
+    let timeout: number;
+
+    if (copyInfo) {
+      timeout = setTimeout(() => setCopyInfo(false), 3000);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [copyInfo]);
+
   return (
     <Flex
       backgroundColor={`#${hex}`}
-      w='14%'
-      minW='100px'
-      maxW='270px'
+      w="14%"
+      minW="100px"
+      maxW="270px"
       h="150px"
       cursor="pointer"
       transition="all 0.5s ease"
       justifyContent="center"
       alignItems="center"
+      flexDirection="column"
       _hover={{ transform: "scale(1.1)" }}
+      onClick={copyToBuffer}
     >
-      #{hex}
+      <Heading as="h3" fontWeight="500" fontSize="20">
+        #{hex}
+      </Heading>
+      <Heading
+        as="h3"
+        fontWeight="400"
+        fontSize="20"
+        visibility={copyInfo ? "visible" : "hidden"}
+      >
+        Color copied!
+      </Heading>
     </Flex>
   );
 }
